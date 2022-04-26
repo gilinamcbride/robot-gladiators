4f6acf87ce99771e6eb963249f946b9795586d2e
Gilina McBride <gilinamcbride@gmail.com>
2022-04-25 20:47:44 -0400

    add statements to improve UX

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,6 +68,13 @@ var fight = function(enemyName) {
 }; //end of fight function
 
 for(var i = 0; i <enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Wlecome to Robot Gladiators! Round " + ( i + 1 ) );
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
